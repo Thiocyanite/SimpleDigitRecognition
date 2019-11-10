e51a2b23a184bafb63fd85b8413e94584de0e49c
Julia <azotowyv@gmail.com>
2019-11-10 14:24:05 +0100

    Ready GUI

---
+++ guicode.py
@@ -0,0 +1,47 @@
+from tkinter import *
+
+
+class GUI:
+    def __init__(self):
+        self.root = Tk()
+        self.root.title("Digit recogniser")
+        self.root.geometry("500x300")
+        self.leftFrame = Frame(self.root)
+        self.leftFrame.pack(side=LEFT)
+        self.rightFrame = Frame(self.root)
+        self.rightFrame.pack(side=RIGHT)
+        self.bottomFrame = Frame(self.root)
+        self.bottomFrame.pack(side=BOTTOM)
+        self.authorLabel = Label(self.bottomFrame, text="Created by Dominik and Julia", fg="white", bg="black")
+        self.authorLabel.pack(fill=X)
+        self.processButton = Button(self.rightFrame, text="Process", fg="purple", bg="black", command=self.process)
+        self.processButton.pack(side=TOP)
+        self.processReset = Button(self.rightFrame, text="Reset", fg="purple", bg="black", command=self.reset)
+        self.processReset.pack(side=TOP)
+        self.answerNum = Label(self.rightFrame)
+        self.answerNum.pack(side=BOTTOM)
+        self.answer = Label(self.rightFrame, text="Recognised number:")
+        self.answer.pack(side=BOTTOM)
+        self.drawHere = Label(self.leftFrame, text="Please write here:")
+        self.drawHere.pack(side=LEFT)
+        self.canvas = Canvas(self.leftFrame, width=28, height=28)
+        self.canvas.pack(side=RIGHT, expand=YES, fill=BOTH)
+        self.reset()
+        self.canvas.bind("<B1-Motion>", self.paint)
+        self.root.mainloop()
+
+    def process(self):
+        print("Wybrano Process")
+        self.answerNum["text"] = "Num"
+
+    def reset(self):
+        self.canvas.create_rectangle(3, 3, 30, 30, fill="white", )
+
+    def paint(self, event):
+        x1, y1 = (event.x - 1), (event.y - 1)
+        x2, y2 = (event.x + 1), (event.y + 1)
+        self.canvas.create_oval(x1, y1, x2, y2, fill="black")
+
+
+if __name__ == "__main__":
+    gui = GUI()
