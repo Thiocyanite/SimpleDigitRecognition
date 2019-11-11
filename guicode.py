b2a6513b487e7f17acdf55a1548204805a449f4d
Dominik Witczak <domionato@gmail.com>
2019-11-11 20:57:45 +0100

    Neural Netowrk loading

--- guicode.py
+++ guicode.py
@@ -1,7 +1,18 @@
 from tkinter import *
-from skimage import filters, measure, io, transform
+import skimage.io as ski_io
 
-class GUI:
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
+from tensorflow.keras import regularizers
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras import backend as K
+from tensorflow.keras.models import model_from_json
+import cv2
+import numpy as np
+
+class Application:
     def __init__(self):
         self.root = Tk()
         self.root.title("Digit recogniser")
@@ -28,11 +39,34 @@ class GUI:
         self.canvas.pack(side=RIGHT, expand=YES, fill=BOTH)
         self.reset()
         self.canvas.bind("<B1-Motion>", self.paint)
+        self.load_neural_network()
         self.root.mainloop()
 
+    def load_neural_network(self):
+        # load json and create model
+        json_file = open('model.json', 'r')
+        loaded_model_json = json_file.read()
+        json_file.close()
+        self.neural_network = model_from_json(loaded_model_json)
+        # load weights into new model
+        self.neural_network.load_weights("model.h5")
+        print("Loaded model from disk")
+
     def process(self):
-        self.answerNum["text"] = "Num"
         self.canvas.postscript(file="Num.eps",height=27,width=27,x=4,y=4)
+        # read the postscript data
+        data = ski_io.imread("Num.eps")
+        # write a rasterized png file
+        ski_io.imsave("Num.png", data)
+        img = cv2.imread("Num.png")
+        kernel = np.ones((5, 5), np.float32) / 25
+        img = cv2.filter2D(img, -1, kernel)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        img = cv2.resize(img, (28, 28))
+        img = np.reshape(img, [1, 28, 28, 1])
+        print(img.shape)
+        classes = self.neural_network.predict_classes(img)
+        self.answerNum["text"] = classes
 
     def reset(self):
         self.canvas.create_rectangle(3, 3, 33, 33, fill="white")
@@ -44,4 +78,4 @@ class GUI:
 
 
 if __name__ == "__main__":
-    gui = GUI()
+    gui = Application()
