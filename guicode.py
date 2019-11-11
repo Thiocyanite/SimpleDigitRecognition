4709293cc5e073214847077020d4dd8b91d73a1a
Julia <azotowyv@gmail.com>
2019-11-11 19:59:58 +0100

    Drawing with different function; canvas 56x56 instead of 28x28

--- guicode.py
+++ guicode.py
@@ -24,7 +24,7 @@ class GUI:
         self.answer.pack(side=BOTTOM)
         self.drawHere = Label(self.leftFrame, text="Please write here:")
         self.drawHere.pack(side=LEFT)
-        self.canvas = Canvas(self.leftFrame, width=31, height=31)
+        self.canvas = Canvas(self.leftFrame, width=65, height=65)
         self.canvas.pack(side=RIGHT, expand=YES, fill=BOTH)
         self.reset()
         self.canvas.bind("<B1-Motion>", self.paint)
@@ -32,15 +32,16 @@ class GUI:
 
     def process(self):
         self.answerNum["text"] = "Num"
-        self.canvas.postscript(file="Num.eps",height=27,width=27,x=4,y=4)
+        self.canvas.postscript(file="Num.eps",height=55,width=55,x=4,y=4)
+
 
     def reset(self):
-        self.canvas.create_rectangle(3, 3, 33, 33, fill="white")
+        self.canvas.create_rectangle(3, 3, 67, 67, fill="white")
 
     def paint(self, event):
         x1, y1 = (event.x - 1), (event.y - 1)
         x2, y2 = (event.x + 1), (event.y + 1)
-        self.canvas.create_oval(x1, y1, x2, y2, fill="black")
+        self.canvas.create_rectangle([x1, y1, x2, y2], fill="black")
 
 
 if __name__ == "__main__":
